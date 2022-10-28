6ddbbb8529dde56a4293106033f45fe93d7a1b39
greg <egreglee@gmail.com>
2022-10-28 14:05:20 -0700

    standalone Python program to plot raw gpsd log

---
+++ remnav/metadata/cmd/gpsd/mapit.py
@@ -0,0 +1,78 @@
+import os
+import argparse
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import random
+import json
+import folium
+from folium import plugins
+
+def read(logfilename):
+    lat = []
+    lon = []
+    speed = []
+    utc = []
+    eph = []
+    mode = []
+    sky_map = {}
+    with open(logfilename) as log:
+        pos = set()
+        for line in log:
+            line_dict = json.loads(line)
+            if line_dict["class"] == "TPV":
+                if "lat" in line_dict:
+                    pos.add((line_dict["lat"], line_dict['lon'], line_dict['speed']))
+                    lat.append(line_dict['lat'])
+                    lon.append(line_dict['lon'])
+                    utc.append(line_dict['time'])
+                    speed.append(line_dict['speed'])
+                    eph.append(line_dict['eph'])
+                    mode.append(line_dict['mode'])
+            elif line_dict["class"] == "SKY":
+                if "hdop" in line_dict:
+                    sky_map[line_dict['time']] = line_dict['hdop']
+    hdop = []
+    for t in utc:
+        hdop.append(sky_map[t] if t in sky_map else np.NaN)
+    df = pd.DataFrame({"utc": pd.to_datetime(utc), 
+                       "lat": lat, "lon": lon, "eph": eph, "hdop": hdop,
+                       "speed": speed, "mode": mode})
+    return df
+
+def html_map(df):
+    m = folium.Map(location=[df.lat.iloc[1], df.lon.iloc[1]], zoom_start=17)
+    #dt = [date ]
+    for index, row in df.iterrows():
+        tooltip = f'{row["utc"]} {row["speed"]:.1f} m/s, eph {row["eph"]:.1f} m'
+        folium.Circle(
+            radius=1,
+            location=[row["lat"], row["lon"]],
+            popup=tooltip,
+            color="blue",
+            fill=False,
+        ).add_to(m)
+    return m
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("log",
+                        help="logged JSON output of GPSD")
+    parser.add_argument("--html",
+                        default="",
+                        help="HTML output file")
+    args = parser.parse_args()
+    html_filename = args.html
+    if not html_filename:
+        root, _ = os.path.splitext(args.log)
+        html_filename = root + ".html"
+
+    df = read(args.log)
+    m = html_map(df)
+    m.save(html_filename)
+    
+
+if __name__ == "__main__":
+    main()
+            
