407486d4c36beac63af9e4b5b790e287fa9c10d7
greg <egreglee@gmail.com>
2022-12-01 16:43:00 -0800

    Rewrote gpsdol to be unaware of sessions.

--- remnav/metadata/cmd/gpsdol/gpsdol.go
+++ remnav/metadata/cmd/gpsdol/gpsdol.go
@@ -3,101 +3,100 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"html/template"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
-	"time"
 
-	"github.com/google/uuid"
-	experiment "remnav.com/remnav/metadata/experiment"
 	gpsd "remnav.com/remnav/metadata/gpsd"
 	storage "remnav.com/remnav/metadata/storage"
 )
 
+func fileTransfer(gnssSubdir, localRoot, archiveServer, archiveRoot string, script io.Writer) {
+	// Write bash script to copy local session storage.
+	m := map[string]interface{}{
+		"gnssSubdir":    gnssSubdir,
+		"localRoot":     localRoot,
+		"archiveServer": archiveServer,
+		"archiveRoot":   archiveRoot}
+
+	t := `#!/bin/bash
+# Run this script after session is complete
+# and a reliable TCP connection is available.
+#    bash filetransfer.sh hjeng
+if [ "$#" -ne 1 ]; then
+  echo "archive user id required, e.g. hjeng"
+  exit 1
+fi
+ARCHIVE_USER="$1"
+ARCHIVE_SERVER="{{.archiveServer}}"
+ARCHIVE_ROOT="{{.archiveRoot}}"
+GNSS_SUBDIR="{{.gnssSubdir}}"
+LOCAL_ROOT="{{.localRoot}}"
+rsync -arv ${LOCAL_ROOT}/${GNSS_SUBDIR} ${ARCHIVE_USER}@${ARCHIVE_SERVER}:${ARCHIVE_ROOT}
+
+`
+	t_ := template.Must(template.New("").Parse(t))
+	t_.Execute(script, m)
+}
+
 func main() {
 	// Listen to gpsd and write messages to
-	// vehicle_root/session_id/gnss/<YYYYmmddTHHMMSST_<fmtid>.json>
-	// ./gpsdol --session_id <session_id> --config <experiment.json>
-	sessionIdFlag := flag.String("session_id", "", "specify session id")
-	configFlag := flag.String("config", "", "JSON experiment configuration")
+	// vehicle_root/gnss/<YYYYmmddTHHMM_<fmtid>.json>
+	// ./gpsdol
+	vehicleRootFlag := flag.String("vehicle_root",
+		"/home/user/6TB/vehicle/remconnect",
+		"vehicle storage directory")
+	archiveServerFlag := flag.String("archive_server",
+		"96.64.247.70",
+		"IP address of archive server (e.g. rn3)")
+	archiveRootFlag := flag.String("archive_root",
+		"/home/user/6TB/remconnect/archive",
+		"archive storage directory (e.g. on rn3")
+	gpsdAddressFlag := flag.String("gpsd_address",
+		"10.1.10.225:2947",
+		"local address of gpsd server")
 	flag.Parse()
-	if len(*configFlag) == 0 {
-		log.Fatalf("%s: --config required", os.Args[0])
-	}
 
-	var sessionId string
-	if len(*sessionIdFlag) > 0 {
-		sessionId = *sessionIdFlag
-	} else {
-		// Convenience for testing.
-		sessionId = fmt.Sprintf("%s_%s",
-			time.Now().UTC().Format("20060102T150405Z"),
-			uuid.NewString())
-	}
-	log.Printf("%s: session_id %s", os.Args[0], sessionId)
-
-	// Read the configuration JSON.
-	configPath, err := filepath.Abs(*configFlag)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Printf("%s: config file %s", os.Args[0], configPath)
-	config, _ := experiment.Read(configPath)
-
-	if len(config.Description) > 0 {
-		log.Printf("%s: configuration description \"%s\"",
-			os.Args[0], config.Description)
-	}
-
-	// Check config values.
-	if len(config.GNSS.GPSDAddress) == 0 {
-		log.Fatalf("%s: gpsd_address invalid or not found in %s",
-			os.Args[0], configPath)
-	}
-	if len(config.Storage.VehicleRoot) == 0 {
-		log.Fatalf("%s: vehicle_root invalid or not found in %s",
-			os.Args[0], configPath)
-	}
-	info, err := os.Stat(config.Storage.VehicleRoot)
-	if os.IsNotExist(err) || !info.IsDir() {
-		log.Fatalf("%s: vehicle_root %s not found or not a directory",
-			os.Args[0], config.Storage.VehicleRoot)
-	}
-
-	// Make the session directory and GNSS subdirectory.
-	sessionPath := filepath.Join(config.Storage.VehicleRoot, sessionId)
-	GNSSPath := filepath.Join(sessionPath, storage.GNSSSubdir)
-	err = os.MkdirAll(GNSSPath, 0775)
+	// Make the local gnss storage directory if it doesn't exist.
+	gnssPath := filepath.Join(*vehicleRootFlag, storage.RawGNSSSubdir)
+	err := os.MkdirAll(gnssPath, 0775)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	// umask modifies the group permission; fix it.
-	err = os.Chmod(sessionPath, 0775)
+	err = os.Chmod(gnssPath, 0775)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	err = os.Chmod(GNSSPath, 0775)
+	err = os.Chmod(gnssPath, 0775)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	// Make the output file.
-	fmtId := "g000"
-	timestampStr := time.Now().UTC().Format("20060102T150405Z")
-	GNSSFilename := fmt.Sprintf("%s_%s.json",
-		timestampStr,
-		fmtId)
-	GNSSFile, err := os.Create(filepath.Join(GNSSPath, GNSSFilename))
+	// Write script to transfer local session files.
+	scriptFilepath := filepath.Join(gnssPath, "filetransfer.sh")
+	scriptFile, err := os.Create(scriptFilepath)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal(err)
 	}
+	defer scriptFile.Close()
+	fileTransfer(storage.RawGNSSSubdir,
+		*vehicleRootFlag,
+		*archiveServerFlag, *archiveRootFlag,
+		scriptFile)
+	os.Chmod(scriptFilepath, 0775)
+	log.Printf("%s: run this script on vehicle when session is finished and a reliable WiFi or Ethernet connection is available: %s",
+		os.Args[0],
+		scriptFilepath)
 
-	conn, reader := gpsd.Conn(config.GNSS.GPSDAddress)
+	// Set up the connection to gpsd.
+	conn, reader := gpsd.Conn(*gpsdAddressFlag)
 	defer conn.Close()
 
 	gpsd.PokeWatch(conn)
 
-	gpsd.Watch(config.GNSS.GPSDAddress, reader, GNSSFile)
+	gpsd.WatchLogPeriodic(*gpsdAddressFlag, reader, gnssPath)
 }
