35876cdb75242b27fc1c69bc047c8197abb51a91
greg <egreglee@gmail.com>
2023-01-09 14:27:44 -0800

    Obsolete metadata/experiment directory and most of metadata/storage.

---
+++ remnav/metadata/storage/storage.go
@@ -0,0 +1,10 @@
+package storage
+
+// Video files are stored in this subdirectory of the session directory
+var VideoSubdir = "video"
+
+// GNSS files are stored in this subdirectory of the session directory
+var GNSSSubdir = "gnss"
+
+// Raw gnss logs are stored in this subdirectory of the archive directory
+var RawGNSSSubdir = "gnss"
