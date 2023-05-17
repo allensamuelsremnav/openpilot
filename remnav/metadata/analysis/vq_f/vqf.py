16cc68419d2e6d3de25383440ddf416e4ea63337
gopal solanki <gopal.solanki@remnav.com>
2023-05-17 13:17:09 -0700

    intermediate checkin - in_service state machine working for few hundred transactions

--- remnav/metadata/analysis/vq_f/vqf.py
+++ remnav/metadata/analysis/vq_f/vqf.py
@@ -4,6 +4,7 @@ import time
 from operator import itemgetter
 import bisect
 from collections import namedtuple
+from copy import deepcopy
 
 def read_log_file (filename, tuplename):
     """ returns an array (list) of specified namedtuples from the data in the filename"""
@@ -142,74 +143,97 @@ GOOD_QUALITY = 0
 POOR_QUALITY = 2
 WAITING_TO_GO_GOOD = 3
 
-#inital states and input conditions
+# set epoch times and other initial conditions 
 latency_index = 0
 uplink_index = 0
 service_index = 0
-synthesized_service_index = 0
-itr_count = 0
 
+latency_line = log_dic["latency"][latency_index]
+uplink_line = log_dic["uplink"][uplink_index]
+service_line = log_dic["service"][service_index]
+
+next_external_eval_TS = min (latency_line.bp_t2r_receive_TS, uplink_line.queue_size_sample_TS, service_line.service_transition_TS)
+
+bp_t2r_receive_TS = [next_external_eval_TS]*3
+bp_t2r = [30]*3
+est_t2r = [30]*3
+
+channel_service_state = [IN_SERVICE]*3 # att, vz, t-mobile
+service_state_transition = [0]*3
+channel_service_state_x_TS = [next_external_eval_TS]*3
+
+queue_size_monitor_working = [True]*3
+queue_size = [0]*3
+
+channel_quality_state = [GOOD_QUALITY]*3
+
+current_TS = next_external_eval_TS
+channel_quality_state_x_TS = [next_external_eval_TS]*3
+# end of start of processing initialization
+
+itr_count = 0
 while latency_index < len(log_dic["latency"]) and uplink_index < len(log_dic["uplink"]) and service_index < len(log_dic["service"]):
     
+    #
     # set up next clock tick when the state transition should be evaluated
+    #
     latency_line = log_dic["latency"][latency_index]
     uplink_line = log_dic["uplink"][uplink_index]
     service_line = log_dic["service"][service_index]
 
-    next_eval_TS = \
+    # find the log input that needs to evaluated next (earliest TS)
+    next_external_eval_TS = \
         min (latency_line.bp_t2r_receive_TS, uplink_line.queue_size_sample_TS, service_line.service_transition_TS)
     
-    if (uplink_line.queue_size_sample_TS == 1683927091587):
-        print ("reached uplink ts: ", 1683927091587)
-    
-    if (latency_index + uplink_index + service_index) == 0:  # initialization at the start of processing
-        bp_t2r_receive_TS = [next_eval_TS]*3
-        bp_t2r = [30]*3
-        est_t2r = [30]*3
-
-        channel_service_state = [IN_SERVICE]*3 # att, vz, t-mobile
-        channel_service_state_x_TS = [next_eval_TS]*3
-
-        queue_size_monitor_working = [True]*3
-        queue_size = [0]*3
-
-        channel_quality_state = [GOOD_QUALITY]*3
-
-        current_TS = next_eval_TS
-    # end of start of processing initialization
-
+    # now check if pending potential state transitions want to be evaluated earlier
+    internal_clock_tick = 0
+    next_internal_eval_TS = current_TS + 120
     for i in range (3):
-        if (channel_service_state[i] == IN_SERVICE) and (est_t2r[i] <= 120):
-            next_eval_TS = min (next_eval_TS, current_TS + 120 - est_t2r[i] + 1)
+        if (channel_service_state[i] == IN_SERVICE) and (est_t2r[i] < 120):
+            next_internal_eval_TS = min (next_internal_eval_TS, current_TS + 120 - est_t2r[i]) # + 1)
+            internal_clock_tick = 1
 
         if (channel_service_state[i] == WAITING_TO_GO_OUT_OF_SERVICE):
-            next_eval_TS = min (next_eval_TS, current_TS + 5 - others_in_service_for[i] + 1)
+            next_internal_eval_TS = min (next_internal_eval_TS, current_TS + 5 - others_in_service_for[i]) # + 1)
+            internal_clock_tick = 1
+    # if a state transition occurred then evaluate right away in case another channel is waiting 
+    if sum(service_state_transition): 
+        next_internal_eval_TS = current_TS
+        internal_clock_tick = 1
 
-        if (channel_service_state[i] == IN_SERVICE):
-            next_eval_TS = min (next_eval_TS, time_since_last_transition[i])
+    # prioritize internal clock tick by not reading hte external inputs to match the implementation
+    if internal_clock_tick and (next_internal_eval_TS <= next_external_eval_TS):
+        current_TS = next_internal_eval_TS
 
-    current_TS = next_eval_TS
-
-    # external inputs
-    if current_TS == latency_line.bp_t2r_receive_TS:
-        bp_t2r[latency_line.receiving_channel] = latency_line.bp_t2r
-        bp_t2r_receive_TS[latency_line.receiving_channel] = latency_line.bp_t2r_receive_TS
-        latency_index += 1
+    else: # external inputs are read only if no interal pending inputs
+        current_TS = next_external_eval_TS    
+        
+        # read all the nputs with the timestamps = current_TS
+        while (current_TS == latency_line.bp_t2r_receive_TS) and (latency_index < len(log_dic["latency"])):
+            bp_t2r[latency_line.receiving_channel] = latency_line.bp_t2r
+            bp_t2r_receive_TS[latency_line.receiving_channel] = latency_line.bp_t2r_receive_TS
+            latency_index += 1
+            if (latency_index < len(log_dic["latency"])): latency_line = log_dic["latency"][latency_index]
+        
+        while (current_TS == uplink_line.queue_size_sample_TS) and (uplink_index < len(log_dic["uplink"])): 
+            queue_size[uplink_line.channel] = uplink_line.queue_size
+            uplink_index += 1
+            if (uplink_index < len(log_dic["uplink"])): uplink_line = log_dic["uplink"][uplink_index]
+        
+        while (current_TS == service_line.service_transition_TS) and (service_index < len(log_dic["service"])):
+            queue_size_monitor_working[service_line.channel] = service_line.zeroUplinkQueue == 0
+            service_index += 1
+            if (service_index < len(log_dic["service"])): service_line = log_dic["service"][service_index]
     
-    if current_TS == uplink_line.queue_size_sample_TS: 
-        queue_size[uplink_line.channel] = uplink_line.queue_size
-        uplink_index += 1
-    
-    if current_TS == service_line.service_transition_TS:
-        queue_size_monitor_working[service_line.channel] = service_line.zeroUplinkQueue == 0
-        service_index += 1
-    
+    """
+    print ("current_TS: ", current_TS)
+    if (current_TS == 1683927095731):
+        print ("current_TS: ", 1683927095731)
+    """
     # synthesized inputs
     est_t2r = [0]*3
     for i in range (3):
         est_t2r[i] = bp_t2r[i] + (current_TS - bp_t2r_receive_TS[i])
-    
-    num_of_channels_in_service = sum (int (x == IN_SERVICE) for x in channel_service_state)
     
     others_in_service_for = [0]*3
     for i in range (3):
@@ -225,35 +249,49 @@ while latency_index < len(log_dic["latency"]) and uplink_index < len(log_dic["up
     # In_service state machine
     ########################################################################################
     next_channel_service_state = [st for st in channel_service_state]
+    service_state_transition = [0]*3
     for i in range (3):
 
+        num_of_channels_in_service = sum (int (x != OUT_OF_SERVICE) for x in next_channel_service_state)
+
         if channel_service_state[i] == IN_SERVICE:
-            if (queue_size_monitor_working[i] and (queue_size[i] > 10)) or (est_t2r[i] > 120): 
+            if (queue_size_monitor_working[i] and (queue_size[i] > 10)) or (est_t2r[i] >= 120): 
                 # channel needs to go out of service as soon as possible
                 if (num_of_channels_in_service == 3): 
-                    next_channel_service_state[i], channel_service_state_x_TS[i] = \
-                        [OUT_OF_SERVICE, current_TS]
-                elif (num_of_channels_in_service == 2):
-                    next_channel_service_state[i], channel_service_state_x_TS[i] = \
-                        [OUT_OF_SERVICE, current_TS] if others_in_service_for[i] >=5 else \
-                        [WAITING_TO_GO_OUT_OF_SERVICE, channel_service_state_x_TS[i]]
+                    next_channel_service_state[i], channel_service_state_x_TS[i], service_state_transition[i] = \
+                        [OUT_OF_SERVICE, current_TS, 1]
+                elif (num_of_channels_in_service == 2) and (others_in_service_for[i] >=5):
+                    next_channel_service_state[i], channel_service_state_x_TS[i], service_state_transition[i] = \
+                        [OUT_OF_SERVICE, current_TS, 1] 
+                else:
+                    next_channel_service_state[i], channel_service_state_x_TS[i], service_state_transition[i] = \
+                        [WAITING_TO_GO_OUT_OF_SERVICE, channel_service_state_x_TS[i], 0]
                 # if this is the only active channel, then can not go out of service
                 
         elif channel_service_state[i] == WAITING_TO_GO_OUT_OF_SERVICE:
-            if (num_of_channels_in_service > 2) or (others_in_service_for[i] > 5):
-                next_channel_service_state[i], channel_service_state_x_TS[i] = \
-                    [OUT_OF_SERVICE, current_TS]
+            if (num_of_channels_in_service > 2) or (others_in_service_for[i] >= 5):
+                # if the channel has not recovered by now, then proceed to OUT_OF_SERVICE
+                if (queue_size_monitor_working[i] and (queue_size[i] > 10)) or (est_t2r[i] >= 120): 
+                    next_channel_service_state[i], channel_service_state_x_TS[i], service_state_transition[i] = \
+                    [OUT_OF_SERVICE, current_TS, 1]
+                else: # else go back to IN_SERVICE
+                    next_channel_service_state[i], channel_service_state_x_TS[i], service_state_transition[i] = \
+                    [IN_SERVICE, channel_service_state_x_TS[i], 0]
 
         else: # channel_service_state[i] = OUT_OF_SERVICE
             if (not queue_size_monitor_working[i] or (queue_size[i] < 5)) and (est_t2r[i] < 80):
-                next_channel_service_state[i], channel_service_state_x_TS[i] = \
-                    [IN_SERVICE, current_TS]
+                next_channel_service_state[i], channel_service_state_x_TS[i], service_state_transition[i] = \
+                    [IN_SERVICE, current_TS, 1]
     
         # emit output if state change
-        if (next_channel_service_state[i] != WAITING_TO_GO_OUT_OF_SERVICE) and (channel_service_state[i] != next_channel_service_state[i]): 
-            fout.write("x_TS,{TS}, x_ch,{ch}, state,{st}, mon,{m}, qsz,{q}, est_t2r,{t2r}, numChIS,{n}, others,{o}".format (\
+        if service_state_transition[i]: 
+
+            fout.write ("x_TS,{TS}, x_ch,{ch}, state,{st}, mon,{m}, qsz,{q}, est_t2r,{t2r}, \
+            numChIS,{n}, others,{o}, qsz,{qv}, est_t2r,{t2rv}, st,{stv}, nx_st,{nxstv}, stx,{stxv}".format ( \
             TS=channel_service_state_x_TS[i], ch=i, st=next_channel_service_state[i], m=queue_size_monitor_working[i], \
-            q=queue_size[i], t2r=est_t2r[i], n=num_of_channels_in_service, o=others_in_service_for[i]))
+            q=queue_size[i], t2r=est_t2r[i], n=num_of_channels_in_service, o=others_in_service_for[i], \
+            qv=queue_size, t2rv=est_t2r, stv=channel_service_state, nxstv=next_channel_service_state, stxv=service_state_transition))
+
             fout.write("\n")
     
             if itr_count % 1000 == 0: 
@@ -262,9 +300,28 @@ while latency_index < len(log_dic["latency"]) and uplink_index < len(log_dic["up
     # for In_service state machine of each channel
     channel_service_state = [st for st in next_channel_service_state]
 
+    """
     ########################################################################################
     # channel quality state machine
     ########################################################################################
+    next_channel_quality_state = deepcopy(channel_quality_state)
+    for i in range(3):
+
+        if (queue_size_monitor_working[i] == False):
+            channel_quality_state[i] = POOR_QUALITY    
+
+        elif channel_quality_state[i] == GOOD_QUALITY:
+            if (queue_size[i] > 10) or (est_t2r[i] > 120):
+                channel_quality_state[i], channel_service_state_x_TS[i] = [POOR_QUALITY, current_TS]
+        
+        elif channel_quality_state[i] == POOR_QUALITY:
+            
+
+    # for channel quality state machine of each channel
+
+    if itr_count == 200:
+        break
+    """
 # while have not exhausted at least one input log file
 
 fout.close ()
