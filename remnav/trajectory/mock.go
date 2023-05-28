4a8906135a1e14e1c86be45995f86e7a7c07d728
greg <egreglee@gmail.com>
2023-05-28 14:10:25 -0700

    VehicleMock

---
+++ remnav/trajectory/mock.go
@@ -0,0 +1,107 @@
+package trajectory
+
+import (
+	"encoding/json"
+	"log"
+	"time"
+)
+
+// Vehicle mock takes Trajectories and computes TrajectoryApplied messages with
+// realistic RTTs  and application delays.
+
+// RTT is modeled as
+// operator -> vehicle
+// + start control computation (sync with OpenPilot)
+// + estimate application time
+// + vehicle -> operator
+//
+// The first applied message arrives at the operator station after an RTT;
+// on average, subsequent messages arrive at 20 Hz.
+//
+// A rough estimate might be 30 + 50 + 0 + 35 (on average)
+//
+// Application delay is
+// operator -> vehicle
+// + start control computation (sync with OpenPilot)
+// + compute control
+// + sync with OpenPilot controls
+// + control delay, e.g. actuators
+//
+// A guess is 30 + 50/2 + 50/2 + 50/2 + 50
+//
+// The vehicle control loop executes at 20 Hz
+type VehicleMock struct {
+	RTT              int // ms
+	ApplicationDelay int // ms
+	ExecutionPeriod  int // ms
+}
+
+func (m VehicleMock) doit(trajBytes []byte) []byte {
+	var traj Trajectory
+	err := json.Unmarshal(trajBytes, &traj)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var appl TrajectoryApplication
+	appl.Class = ClassTrajectoryApplication
+	appl.Trajectory = traj.Requested
+	appl.Applied = (int64)(m.ApplicationDelay)*1000 + traj.Requested
+	applBytes, err := json.Marshal(appl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return applBytes
+}
+
+// Generate TrajectoryApplied messages for a stream of Trajectory messages.
+func (m VehicleMock) run(trajectories <-chan []byte) <-chan []byte {
+	applicationCh := make(chan []byte)
+
+	executionDuration := time.Duration(m.ExecutionPeriod) * time.Millisecond
+
+	go func() {
+		defer close(applicationCh)
+		var queue [][]byte
+		queue = append(queue, <-trajectories)
+		// Force RTT delay for first delivered TrajectoryApplication.
+		startTimer := time.NewTimer(time.Duration(m.RTT) * time.Millisecond)
+	startLoop:
+		for {
+			select {
+			case traj := <-trajectories:
+				queue = append(queue, traj)
+			case <-startTimer.C:
+				tb := queue[0]
+				queue = queue[1:]
+				applicationCh <- m.doit(tb)
+				break startLoop
+			}
+		}
+
+		// Force subsequent reports every ExecutionPeriod.
+		var executionTimer = time.NewTimer(executionDuration)
+		trajOk := true
+		for {
+			var traj []byte
+			select {
+			case traj, trajOk = <-trajectories:
+				if !trajOk {
+					trajectories = nil
+					break
+				}
+				queue = append(queue, traj)
+			case <-executionTimer.C:
+				if len(queue) > 0 {
+					tb := queue[0]
+					queue = queue[1:]
+					applicationCh <- m.doit(tb)
+					executionTimer.Reset(executionDuration)
+				}
+				if len(queue) == 0 && !trajOk {
+					return
+				}
+			}
+		}
+	}()
+	return applicationCh
+}
---
+++ remnav/trajectory/mock_test.go
@@ -0,0 +1,55 @@
+package trajectory
+
+import (
+	"encoding/json"
+	"fmt"
+	"math"
+	"testing"
+	"time"
+)
+
+func TestBasic(t *testing.T) {
+	var mock VehicleMock
+	mock.RTT = 115
+	mock.ApplicationDelay = 205
+	mock.ExecutionPeriod = 50
+	trajectories := make(chan []byte)
+	applications := mock.run(trajectories)
+	go func(n int) {
+		defer close(trajectories)
+		ticker := time.NewTicker(time.Duration(50) * time.Millisecond)
+		for i := 0; i < 10; i++ {
+			select {
+			case <-ticker.C:
+				var traj Trajectory
+				traj.Class = ClassTrajectory
+				traj.Requested = time.Now().UnixMicro()
+				bytes, err := json.Marshal(traj)
+				if err != nil {
+					t.Fatal(err)
+				}
+				trajectories <- bytes
+			}
+		}
+	}(10)
+	for applBytes := range applications {
+		now := time.Now().UnixMicro()
+		var appl TrajectoryApplication
+		err := json.Unmarshal(applBytes, &appl)
+		if err != nil {
+			t.Error(err)
+		}
+		fmt.Printf("RTT %d μs = %d - %d, applied - trajectory = %d μs = %d - %d\n", now-appl.Trajectory, now, appl.Trajectory, appl.Applied-appl.Trajectory, appl.Applied, appl.Trajectory)
+		eps := 7500.0
+		checkRTT := math.Abs((float64)(now - appl.Trajectory - int64(mock.RTT*1000)))
+		if checkRTT > eps {
+			t.Fatalf("%f, now-appl.Trajectory %d = %d - %d > %f", checkRTT, now-appl.Trajectory, now, appl.Trajectory, eps)
+		}
+		checkDelay := math.Abs((float64)(appl.Applied - appl.Trajectory - int64(mock.ApplicationDelay*1000)))
+		epsDelay := 0.0 // Delay is fixed.
+		if checkDelay > epsDelay {
+			t.Fatalf("%f, appl.Applied-appl.Trajectory %d = %d - %d > %f", checkDelay, appl.Applied-appl.Trajectory, appl.Applied, appl.Trajectory, epsDelay)
+		}
+
+	}
+}
