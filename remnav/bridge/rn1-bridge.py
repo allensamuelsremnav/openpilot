443c7c1a681bf7fbfe5327702b2ceda284a00f22
Allen Samuels <allen.samuels@remnav.com>
2023-08-13 16:19:23 -0700

    more proto stuff

--- remnav/bridge/rn1-bridge.py
+++ remnav/bridge/rn1-bridge.py
@@ -15,11 +15,12 @@ import psutil
 parser = argparse.ArgumentParser()
 parser.add_argument('-station_ip',help="IP Address of operator station", default="127.0.0.1")
 parser.add_argument('-station_port', help="Port number of operator station", type=int, default=6002)
+parser.add_argument('-vehicle_ip', help='IP Address of OpenPilot', default='127.0.0.1')
+parser.add_argument('-mpc_port', help='Port number for vehicle MPC controller', type=int, default=6380)
+parser.add_argument('-pid_port', help='Port number for vehciel PID controller', type=int, default=6379)
 parser.add_argument('-i', '-interfaces', metavar='InterfaceName', type=str, nargs='+',
                     help='Names of local NIC interfaces')
 args = parser.parse_args()
-
-vehicle_address = ('127.0.0.1', 6379)
 
 def get_ip_cross(interface: str) -> str:
     """
@@ -56,11 +57,12 @@ def make_message_for_index(index, msg):
 def make_beacon_for_index(index):
     return make_message_for_index(index,'{"class":"TRAJECTORY_APPLICATION","trajectory":0,"applied":0,"log":0}\n')
 
-vehicle_to_bridge = None
-
 class VehicleToBridge:
-    def __init__(self):
+    def __init__(self, name, vehicle_address, handle_reply_function):
         self.run = True
+        self.name = name
+        self.vehicle_address = vehicle_address
+        self.handle_reply_function = handle_reply_function
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         self.thread = threading.Thread(target=self.runner)
@@ -72,30 +74,43 @@ class VehicleToBridge:
         self.thread.join()
 
     def runner(self):
-        logging.info(f"Waiting to connect to vehicle at {vehicle_address}")
+        logging.info(f"Waiting to connect {self.name} controller at {self.vehicle_address}")
         while self.run:
             try:
-                self.socket.connect(vehicle_address)
+                self.socket.connect(self.vehicle_address)
             except ConnectionRefusedError:
                 continue
             except socket.timeout:
                 continue
-            logging.info(f"Established connection to vehicle from address {self.socket.getpeername()}")
+            logging.info(f"Established connection to {self.name} at address {self.socket.getpeername()}")
             while self.run:
                 try:
                     buffer = self.socket.recv(1024).decode('utf-8')
+                    #
+                    # Need to accumulate partial messages and parse out only till newlines to pass into handle function
+                    #
                     for msg in buffer.split('\n'):
                         if msg.startswith('<'):
                             tag = msg[1:].split('>')[0]
-                            logging.info(f"Received Vehicle Tag: {tag}")
-                            vm = f'{"class":"TRAJECTORY_APPLICATION","trajectory":{tag},"applied":{int(time.time())},"log":0}'
-                            StationToBridge.broadcast_reply(vm)
-                        else:
-                            logging.info(f"Bad message from vehicle ignored: {msg}")
+                            reply = self.handle_reply_function(tag)
                 except socket.timeout:
                     continue
                 except ConnectionAbortedError:
                     break
+
+def make_MPC_reply(tag):
+    reply = {
+        'class': 'TRJECTORY_APPLICATION',
+        'trajectory': int(tag),
+        'applied': time.time_ns()//1000,
+        'log': 0
+    }
+    logging.info(f"Received MPC Vehicle Tag: {tag}, Reply:{reply}")
+    StationToBridge.broadcast_reply(json.dumps(reply))
+
+def make_PID_reply(tag):
+    logging.info(f"Received PID Vehicle Tag: {tag}")
+    pass
 
 
 class StationToBridge:
@@ -122,7 +137,8 @@ class StationToBridge:
             s.socket.sendto(make_message_for_index(s.index, m), station_address)
 
     def listener(self):
-        global last_rcv_timestamp, vehicle_to_bridge
+        last_trajectory_timestamp = 0
+        last_g920_timestamp = 0
         logging.info(f"Sending beacon from {self.interface} to {station_address}")
         b = make_beacon_for_index(self.index)
         print("Bytes are: ",b)
@@ -134,44 +150,45 @@ class StationToBridge:
                 message, address = self.socket.recvfrom(1024)
                 logging.info(f"Received on interface {self.interface} from {address} msg: {message}")
                 msg = json.loads(message)
+                request_timestamp = int(msg['requested'])
                 if msg['class'] == "TRAJECTORY":
-                    request_timestamp = int(msg['requested'])
-                    if request_timestamp > last_rcv_timestamp:
-                        last_rcv_timestamp = request_timestamp
-                        last_rcv_msg_time = time.time()
-                        if not vehicle_to_bridge:
-                            logging.info(f"Opening connection to vehicle")
-                            vehicle_to_bridge = VehicleToBridge()
-                        StationToBridge.broadcast_reply(self.generate_reply(msg))
+                    if request_timestamp > last_trajectory_timestamp:
+                        last_trajectory_timestamp = request_timestamp
+                        StationToBridge.handle_trajectory_message(self.generate_reply(msg))
                     else:
                         logging.info(f"Discarding message to {self.interface}, duplicate")
-            except socket.timeout:
-                if vehicle_to_bridge:
-                    logging.info("Timeout on interface {self.interface}, closing vehicle connection")
-                    vehicle_to_bridge.stop()
-                    vehicle_to_bridge = None
+                elif msg['class'] == "G920":
+                    if request_timestamp > last_g920_timestamp:
+                        last_g920_timestamp = request_timestamp
+                        StationToBridge.handle_g920_message(self.generate_reply(msg))
+                    else:
+                        logging.info(f"Discarding message to {self.interface}, duplicate")
 
+
+
+            except socket.timeout:
                 logging.info(f"Sending beacon from {self.interface} to {station_address}")
                 self.socket.sendto(make_beacon_for_index(self.index), station_address)
 
-    def generate_reply(self, msg):
+    def handle_trajectory_message(self, msg):
+        ''' Handle receipt of a valid trajectory message. '''
         curvature = msg['curvature']
         if math.isinf(curvature):
-            steering = 0
+            radius = 0
+        elif curvature == 0:
+            radius = math.inf()
         else:
-            '''Compute steering from Bicycle Model'''
             radius = 1 / curvature
-        reply = {
-            'class':'TRAJECTORY_ADJUSTMENT',
-            'trajectory': msg['requested'],
-            'applied': msg['requested'],
-            'log': 0
-            }
-        return json.dumps(reply)
+        #
+        # Make message in format for MPC controller
+        # 
+        msg = f"<{msg['received']}>s {radius}"
+
         
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)            
 logging.info(f"Starting up")            
-vehicle_to_bridge = VehicleToBridge()
+vehicle_mpc = VehicleToBridge('MPC', (args['vehicle_address'], args['mpc_port']), MPC_reply_function)
+vehicle_pid = VehicleToBridge('PID', (args['vehicle_address'], args['pid_port']), PID_reply_function)
 station_to_bridge = []
 addrs = psutil.net_if_addrs()
 print("Network interfaces are:", addrs.keys())
