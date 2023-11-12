60b4ce6540b124c5dbdcef2705dfe3eaf30b3aef
Allen Samuels <allen.samuels@remnav.com>
2023-11-12 08:43:14 -0800

    Fix 0 length case

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -618,8 +618,10 @@ class Controls:
                                                                                        lat_plan.psis,
                                                                                        lat_plan.curvatures,
                                                                                        lat_plan.curvatureRates)
-      self.desired_curvature = lat_plan.curvatures[0] #remnav
-      self.desired_curvature_rate = lat_plan.curvatureRates[0] #remnav
+      if len(lat_plan.curvatures) > 0: # remnav
+        self.desired_curvature = lat_plan.curvatures[0] #remnav
+      if len(lat_plan.curvatureRates) > 0: #remanv
+        self.desired_curvature_rate = lat_plan.curvatureRates[0] #remnav
       actuators.steer, actuators.steeringAngleDeg, lac_log = self.LaC.update(CC.latActive, CS, self.VM, lp,
                                                                              self.last_actuators, self.steer_limited, self.desired_curvature,
                                                                              self.desired_curvature_rate, self.sm['liveLocationKalman'])
