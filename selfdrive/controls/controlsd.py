f435a640fa91fdcfeabd673a5c9b6bb40edcf133
Allen Samuels <allen.samuels@remnav.com>
2023-11-25 08:35:42 -0800

    Bring out previous version of acuators.accel into carState message

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -611,6 +611,7 @@ class Controls:
       pid_accel_limits = self.CI.get_pid_accel_limits(self.CP, CS.vEgo, self.v_cruise_helper.v_cruise_kph * CV.KPH_TO_MS)
       t_since_plan = (self.sm.frame - self.sm.rcv_frame['longitudinalPlan']) * DT_CTRL
       actuators.accel = self.LoC.update(CC.longActive, CS, long_plan, pid_accel_limits, t_since_plan)
+      CC.gasDEPRECATED = actuators.accel
       actuators.accel = self.hijacker.modify(actuators.accel, CS.vEgo) # remnav
 
       # Steering PID loop and lateral MPC
