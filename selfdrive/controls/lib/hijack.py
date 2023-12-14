abfedf9422d60c68e6cde2b39195772ad4274424
Allen Samuels <allen.samuels@remnav.com>
2023-12-13 21:29:18 -0800

    fix json return

--- selfdrive/controls/lib/hijack.py
+++ selfdrive/controls/lib/hijack.py
@@ -288,7 +288,7 @@ class Hijacker:
           result += b'too small radius'
         else:
           result += self.setSteer(math.asin(self.wheelBase / radius))
-          result = self.rmstate.getState()
+          result = '{"uimode":"' + self.rmstate.getState() + '"}'
       except ValueError:
         result += b'Syntax error:' + sline[1]
     elif sline[0] == b'H':
