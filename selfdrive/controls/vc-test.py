df85e81003adb01b08ab3015a19ed1e9ab00762e
Allen Samuels <allen.samuels@remnav.com>
2024-12-15 10:52:58 -0800

    add missing files

---
+++ selfdrive/controls/vc-test.py
@@ -0,0 +1,55 @@
+#! /usr/bin/python3
+import socket, threading, json, time
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(('', 0))
+print(f"Bound UDP Port to {s.getsockname()}")
+dest_address=(('localhost', 7777))
+
+def listener():
+    print("Waiting for UDP response")
+    while True:
+        o, address = s.recvfrom(1500)
+        print(f"From {address}: {o}")
+
+x = threading.Thread(target=listener).start()
+
+accel = 0
+steer = 0
+wan_status = 'normal'
+request_enable = False
+
+def timestamp():
+    return int(time.time() * 1000)
+message_id = 3
+
+while True:
+    line = input(">")
+    if line[0] == 'u':
+        accel = accel + .1
+    elif line[0] == 'd':
+        accel = accel - .1
+    elif line[0] == 'l':
+        steer = steer - .1
+    elif line[0] == 'r':
+        steer = steer + .1
+    elif line[0] == 'E':
+        request_enable = True
+    elif line[0] == 'D':
+        request_enable = False
+    else:
+        pass
+
+    # Build a message
+
+    msg = {
+        'timestamp': timestamp(),
+        'message_id': message_id,
+        'acceleration': accel,
+        'steering': steer,
+        'wan_status': wan_status,
+        'request_enable': request_enable,
+    }
+
+    print(f"Sending to:{dest_address}: {json.dumps(msg)}")
+    s.sendto(json.dumps(msg).encode(), dest_address)
